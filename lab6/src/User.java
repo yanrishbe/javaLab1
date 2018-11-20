4652a7b75a96af3e0d955aad25b6063e57f9d2a7
Yana <38540081+yanrishbe@users.noreply.github.com>
2018-11-20 10:54:07 +0300

    Update User.java

--- lab6/src/User.java
+++ lab6/src/User.java
@@ -1,7 +1,3 @@
-//когда метод возвращает объект и нам не нужно
-// его куда-то сохранить(в переменную) то необязательно присваивать
-// его какой-то переменной и тогда можно вызывать таким способом
-
 public class User {
     public static void main(String[] args) {
         Reader shop = new Reader ("Grocery store supply registration");
